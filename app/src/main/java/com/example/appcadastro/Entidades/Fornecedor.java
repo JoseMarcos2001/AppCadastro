cd76348508f5667fdabe75fd3cbf6abf5ba95eff
José Marcos <802.275@alunos.unigran.br>
2022-10-18 01:19:09 -0400

    entidades

---
+++ app/src/main/java/com/example/appcadastro/Entidades/Fornecedor.java
@@ -0,0 +1,37 @@
+package com.example.appcadastro.Entidades;
+
+public class Fornecedor {
+    private String nome;
+    private String numero;
+    private Integer id;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "nome='" + nome + '\'' +
+                ", numero='" + numero;
+    }
+}
---
+++ app/src/main/java/com/example/appcadastro/Entidades/Produto.java
@@ -0,0 +1,37 @@
+package com.example.appcadastro.Entidades;
+
+public class Produto {
+    private String nome;
+    private String marca;
+    private Integer id;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "nome='" + nome + '\'' +
+                ", marca='" + marca;
+    }
+}
